ebfdda9bb3c478fb2b3a79da48b3170ecf24916a
JwalaVarshaReddy <jwalavarshareddy2001@gmail.com>
2026-03-17 20:16:16 -0400

    add the sqlite database

--- Backendwithsqlite.py
+++ Backendwithsqlite.py
@@ -1,8 +1,8 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage,HumanMessage
 from langchain_groq import ChatGroq
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph.message import add_messages
 from dotenv import load_dotenv
 import sqlite3
@@ -19,8 +19,10 @@ def chat_node(state: ChatState):
     response = llm.invoke(messages)
     return {"messages": [response]}
 
+con=sqlite3.connect("chatbot.db",check_same_thread=False)
+
 # Checkpointer
-checkpointer = InMemorySaver()
+checkpointer = SqliteSaver(conn=con)
 
 graph = StateGraph(ChatState)
 graph.add_node("chat_node", chat_node)
@@ -28,3 +30,9 @@ graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(all_threads)
