2d826c32d04411ab0f2e57a2e1f41b52463f844e
JwalaVarshaReddy <jwalavarshareddy2001@gmail.com>
2026-03-18 15:37:26 -0400

    Added observability using Langsmith

--- streamlit_frontend_sqlite.py
+++ streamlit_frontend_sqlite.py
@@ -3,19 +3,15 @@ from Backendwithsqlite import chatbot,retrieve_all_threads
 from langchain_core.messages import HumanMessage
 import uuid
 
-# st.session_state -> dict -> 
-CONFIG = {'configurable': {'thread_id': 'thread-1'}}
-
-###################################Utility Functions##############################################
 def generate_thread_id():
-    thread_id=uuid.uuid4()
+    thread_id = uuid.uuid4()
     return thread_id
 
 def reset_chat():
-    thread_id=generate_thread_id()
-    st.session_state['thread_id']=thread_id
+    thread_id = generate_thread_id()
+    st.session_state['thread_id'] = thread_id
     add_thread(st.session_state['thread_id'])
-    st.session_state['message_history']=[ ]
+    st.session_state['message_history'] = []
 
 def add_thread(thread_id):
     if thread_id not in st.session_state['chat_threads']:
@@ -27,30 +23,27 @@ def load_conversation(thread_id):
     return state.values.get('messages', [])
 
 
+# **************************************** Session Setup ******************************
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
 
 if 'thread_id' not in st.session_state:
-    st.session_state['thread_id']=generate_thread_id()
+    st.session_state['thread_id'] = generate_thread_id()
 
-if "chat_threads" not in st.session_state:
-    st.session_state['chat_threads']=retrieve_all_threads()
+if 'chat_threads' not in st.session_state:
+    st.session_state['chat_threads'] = retrieve_all_threads()
 
 add_thread(st.session_state['thread_id'])
 
-# loading the conversation history
-for message in st.session_state['message_history']:
-    with st.chat_message(message['role']):
-        st.text(message['content'])
-####################################################Sidebar########################################
-# st.session_state -> dict -> 
-CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
 
+# **************************************** Sidebar UI *********************************
 
-st.sidebar.title("LangGraph Chatbot")
-if st.sidebar.button("New Chat"):
+st.sidebar.title('LangGraph Chatbot')
+
+if st.sidebar.button('New Chat'):
     reset_chat()
-st.sidebar.header("My Conversations")
+
+st.sidebar.header('My Conversations')
 
 for thread_id in st.session_state['chat_threads'][::-1]:
     if st.sidebar.button(str(thread_id)):
@@ -67,8 +60,14 @@ for thread_id in st.session_state['chat_threads'][::-1]:
             temp_messages.append({'role': role, 'content': msg.content})
 
         st.session_state['message_history'] = temp_messages
-#{'role': 'user', 'content': 'Hi'}
-#{'role': 'assistant', 'content': 'Hi=ello'}
+
+
+# **************************************** Main UI ************************************
+
+# loading the conversation history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
 
 user_input = st.chat_input('Type here')
 
@@ -78,6 +77,16 @@ if user_input:
     st.session_state['message_history'].append({'role': 'user', 'content': user_input})
     with st.chat_message('user'):
         st.text(user_input)
+
+    #CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+
+    CONFIG = {
+        "configurable": {"thread_id": st.session_state["thread_id"]},
+        "metadata": {
+            "thread_id": st.session_state["thread_id"]
+        },
+        "run_name": "chat_turn",
+    }
 
     # first add the message to message_history
     with st.chat_message('assistant'):
